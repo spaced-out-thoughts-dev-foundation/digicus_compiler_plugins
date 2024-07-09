a8a0b037e14fac50be7e89fcd30c9fb3fba6201f
Rob Durst <me@robdurst.com>
2024-07-08 22:17:10 -0600

    0.1.1 with more telemetry

--- lib/digicus_web_backend/compiler.rb
+++ lib/digicus_web_backend/compiler.rb
@@ -9,7 +9,9 @@ module DigicusWebBackend
     attr_reader :contract
 
     def initialize(dtr_code)
+      start = Time.now
       @contract = DTRCore::Contract.from_dtr_raw(dtr_code)
+      puts "[COMPILE]: { name: digicus_web_backend, type: backend, time: #{Time.now - start}, phase: 'Contract-from-dtr-raw' }"
     end
 
     def self.from_dtr(dtr_code)
@@ -17,13 +19,18 @@ module DigicusWebBackend
     end
 
     def from_dtr
-      {
+      start = Time.now
+      result = {
         contract_name:,
         contract_state:,
         contract_interface:,
         contract_user_defined_types:
 
-      }.to_json
+      }
+
+      puts "[COMPILE]: { name: digicus_web_backend, type: backend, time: #{Time.now - start}, phase: 'Contract-to-json' }"
+
+      result.to_json
     end
 
     private
